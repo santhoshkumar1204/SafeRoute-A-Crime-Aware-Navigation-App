8d021fa3425d50f10792b04cc2924aa9f22f94d0
Sana <shsf101112@gmail.com>
2026-02-21 15:00:54 +0530

    Added flutter_map and updated Firebase dependencies

--- saferoute/macos/Flutter/GeneratedPluginRegistrant.swift
+++ saferoute/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,7 @@ import cloud_firestore
 import connectivity_plus
 import firebase_auth
 import firebase_core
-import firebase_messaging
-import firebase_storage
 import flutter_secure_storage_macos
-import geolocator_apple
 import google_sign_in_ios
 import shared_preferences_foundation
 import url_launcher_macos
@@ -22,10 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
--- saferoute/windows/flutter/generated_plugins.cmake
+++ saferoute/windows/flutter/generated_plugins.cmake
@@ -7,9 +7,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
   firebase_auth
   firebase_core
-  firebase_storage
   flutter_secure_storage_windows
-  geolocator_windows
   url_launcher_windows
 )
 
